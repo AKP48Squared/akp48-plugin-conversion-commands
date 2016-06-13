a0c4aabbccd49fcd5e8a2c255c36bdf30a10aa7a
Austin Peterson <austin@akpwebdesign.com>
2016-06-13 10:32:14 -0500

    Update to new plugin and context systems.

--- main.js
+++ main.js
@@ -5,40 +5,37 @@ const Qty = require('js-quantities');
 
 class Conversion extends global.AKP48.pluginTypes.MessageHandler {
   constructor(AKP48) {
-    super('Conversion', AKP48);
+    super(AKP48, 'Conversion');
   }
 }
 
-Conversion.prototype.handleCommand = function (message, context, res) {
-  global.logger.silly(`${this._pluginName}: Received command.`);
+Conversion.prototype.handleCommand = function (context) {
+  global.logger.silly(`${this.name}: Received command.`);
 
-  // prepare text.
-  context.originalText = context.text;
-  var text = context.text.split(' ');
-  var command = text[0];
-  //text is now an array of all the arguments the user sent.
-  text.shift();
+  var text = context.rawArgs();
+  var command = context.command();
   var responses = [];
 
   if(typeof ConvertBase[command] === 'function') {
-    global.logger.silly(`${this._pluginName}: Responding to ${command} command.`);
+    global.logger.silly(`${this.name}: Responding to ${command} command.`);
     for (var i = 0; i < text.length; i++) {
       responses.push(`${text[i]} => ${ConvertBase[command](text[i])}`);
     }
 
-    res(responses.join(', '));
+    return context.reply(responses.join(', '));
   }
 
   if(typeof ConvertTemp[command] === 'function') {
-    global.logger.silly(`${this._pluginName}: Responding to ${command} command.`);
+    global.logger.silly(`${this.name}: Responding to ${command} command.`);
     for (var i = 0; i < text.length; i++) {
       responses.push(`${ConvertTemp[command](text[i])}`);
     }
-    res(responses.join(', '));
+    return context.reply(responses.join(', '));
   }
 
   //all-in-one solution time.
   if(command === 'convert') {
+    global.logger.silly(`${this.name}: Responding to convert command.`);
     var to = text[0];
     text.shift();
     try {
@@ -54,10 +51,8 @@ Conversion.prototype.handleCommand = function (message, context, res) {
       responses.push(`Error! ${e.message}.`);
     }
 
-    res(responses.join(', '));
+    return context.reply(responses.join(', '));
   }
 };
 
 module.exports = Conversion;
-module.exports.type = 'MessageHandler';
-module.exports.pluginName = 'conversion-commands';
